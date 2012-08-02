cf40995704b9548911736abf66e21aa35c15d1f1
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2012-08-03 00:04:36 +0200

    Finished a complete automated documentation!!

--- gap/InstallMethodWithDocumentation.gd
+++ gap/InstallMethodWithDocumentation.gd
@@ -25,7 +25,7 @@ DeclareGlobalVariable( "AUTOMATIC_DOCUMENTATION" );
 
 ##  <#GAPDoc Label="CreateAutomaticDocumentation">
 ##  <ManSection>
-##    <Func Arg="package_name, documentation_file, path_to_xml_file[,section_intros]" Name="CreateAutomaticDocumentation"/>
+##    <Func Arg="package_name, documentation_file, path_to_xml_file,create_full_docu[,section_intros]" Name="CreateAutomaticDocumentation"/>
 ##    <Returns><C>true</C></Returns>
 ##    <Description>
 ##      This is the main method of the package. After loading the package, run it with the name of the pacckage
@@ -35,6 +35,8 @@ DeclareGlobalVariable( "AUTOMATIC_DOCUMENTATION" );
 ##      It will produce several files out of the
 ##      Declare*WithDocumentation declarations you have used in your package <A>package_name</A>, and one named AutomaticDocumentationMainFile.xml,
 ##      which you can simply include to your documentation.
+##      <A>create_full_docu</A> can either be true or false. If true, a full documentation with title file is created. The only thing left
+##      for you to do is run GAPDoc and provide a bibliography.
 ##      <A>section_intros</A> is optional, it must be a list containing lists of of either two or three strings. If two are given, first one must be
 ##      a chapter title, with underscores instead of spaces, and the second one a string which will be displayed in the documentation at the beginning of
 ##      the chapter. If three are given, first one must be a chapter, second a section, third the description.
@@ -44,8 +46,13 @@ DeclareGlobalVariable( "AUTOMATIC_DOCUMENTATION" );
 ##
 DeclareGlobalFunction( "CreateAutomaticDocumentation" );
 
+##
 ## This function creates a title file. It must be called with the package name and the path to doc files.
 DeclareGlobalFunction( "CreateTitlePage" );
+
+##
+## This function creates the main page. Do not call it out of context.
+DeclareGlobalFunction( "CreateMainPage" );
 
 ##
 ## This function is for internal use only.
