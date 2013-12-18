606693c56e63b430bbb11c1fea5f3d9599312420
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2013-12-18 20:53:12 +0100

    Docu for worksheets.

--- gap/AutoDocMainFunction.gd
+++ gap/AutoDocMainFunction.gd
@@ -61,88 +61,9 @@ DeclareGlobalFunction( "CreateMakeTest" );
 #! @Section Worksheets
 
 #! @Description
-#!  This function takes a filename and returns a complete GAPDoc document created
-#!  out of the file. All AutoDoc commands can be used to create such a file. Also
-#!  there are some special tags, which have only effect in files if the files are parsed with
-#!  this command. Those commands are:
-#!  <List>
-#!  <Mark>Title <A>title</A></Mark>
-#!  <Item>
-#!    This adds a title to the document
-#!  </Item>
-#!  <Mark>Author <A>author</A></Mark>
-#!  <Item>
-#!    This adds an author to the document.
-#!  </Item>
-#!  <Mark>Dependency <A>packagename</A>, <A>version</A></Mark>
-#!  <Item>
-#!   This adds a dependency to the worksheet which will be
-#!   checked at compile time. If your GAP does not provide
-#!   the correct <A>version</A> of the package <A>packagename</A>
-#!   the command will give an error. If you only want to compile
-#!   the worksheet, you can continue. A possible <A>version</A> is >=20.
-#!  </Item>
-#!  <Mark>Date <A>date</A></Mark>
-#!  <Item>
-#!  Adds the string <A>date</A> as a date to the titlepage of the pdf.
-#!  </Item>
-#!  <Mark>Abstract</Mark>
-#!  <Item>
-#!  Adds the following lines as abstract to the title of the worksheet.
-#!  </Item>
-#!  <Mark>Acknowledgement</Mark>
-#!  <Item>
-#!  Adds the following lines as acknowledgement to the title of the worksheet.
-#!  </Item>
-#!  <Mark>URL <A>url</A></Mark>
-#!  <Item>
-#!  Adds the URL <A>url</A> as a link to the titlepage of the worksheet.
-#!  This is done in the TitleComment section of the TitlePage.
-#!  </Item>
-#!  </List>
-#!  Note that some commands have no effect, i.e. the level command.
-#! The options are as follows.
-#! <List>
-#! <Mark>BookName</Mark>
-#! <Item>
-#! The option BookName is an optional string, specifying the name of the manual book.
-#! If there is no BookName, the title is used. If there is no title, the filename of
-#! the first file is used.
-#! </Item>
-#! <Mark>TestFile</Mark>
-#! <Item>
-#! If TestFile is set to false, no testfile is produced. If TestFile is a string,
-#! then it is used as name of the testfile. If nothing is given, the testfile will be
-#! maketest.g.
-#! </Item>
-#! <Mark>TestFileOutputFolder</Mark>
-#! <Item>
-#! Defines an output folder for a testfile. If the string "current" is given,
-#! the current folder will be used to output the testfile. If nothing is given,
-#! OutputFolder will be used.
-#! </Item>
-#! <Mark>OutputFolder</Mark>
-#! <Item>
-#! All files will be stored in OutputFolder. If OutputFolder is not given, the folder of the first file will be used.
-#! </Item>
-#! <Mark>TestFileCommands</Mark>
-#! <Item>
-#! String or list of strings to be added at beginning of testfile.
-#! </Item>
-#! <Mark>Bibliography</Mark>
-#! <Item>
-#! Path to a bibliography file.
-#! </Item>
-#! <Mark>CreateIndex</Mark>
-#! <Item>
-#!  Enables the index if set to true. Please enable it only if you have something
-#!  to index. It will create an error otherwise.
-#! </Item>
-#! <Mark>EntityList</Mark>
-#! <Item>
-#!  Works like the entities part in the AutoDoc command.
-#! </Item>
-#! </List>
-#! 
-#! @Arguments list_of_filenames : BookName, TestFile, OutputFolder, TestFileCommands, Bibliography
+#!  This function works exactly like the AutoDoc command, except that no package is needed to create a
+#!  worksheet. It takes the sampe optional records as the AutoDoc-command, so please refer this command
+#!  for a full list.
+#!  It's only optional argument is a (list of) filenames, which are then scanned by the AutoDoc parser.
+#! @Arguments list_of_filenames : options
 DeclareGlobalFunction( "AutoDocWorksheet" );
