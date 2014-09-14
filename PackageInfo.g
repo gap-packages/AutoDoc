gap-packages/AutoDoc

commit 96cecb58fe284bda9093778f7a1aad12793ca558

Fixed bug for empty nodes.
A chapter with only non-empty subsections but no non-empty section was falsely
considered empty.

PackageInfo.g
@@ -6,7 +6,7 @@ PackageName := "AutoDoc",
 Subtitle := "Generate documentation from GAP source code",
 
 Version := Maximum( [
-  "2014.06.26", ## Sebas' version
+  "2014.08.12", ## Sebas' version
 ## This line prevents merge conflicts
   "2014.03.27", ## Max' version
 ## This line prevents merge conflicts

gap/DocumentationTree.gi
@@ -245,8 +245,7 @@ InstallMethod( DocumentationStructurePart,
     structure_obj.content := [ ];
     
     ObjectifyWithAttributes( structure_obj, type,
-                             Name, obj_name,
-                             IsEmptyNode, true
+                             Name, obj_name
                            );
     
     tree!.nodes_by_name.( Name( structure_obj ) ) := structure_obj;
@@ -439,8 +438,6 @@ InstallMethod( Add,
     
     Add( insert_node!.content, node );
     
-    ResetFilterObj( insert_node, IsEmptyNode );
-    
 end );
 
 ##
@@ -450,8 +447,6 @@ InstallMethod( Add,
   function( insert_node, string )
     
     Add( insert_node!.content, string );
-    
-    ResetFilterObj( insert_node, IsEmptyNode );
     
 end );
 
@@ -568,6 +563,30 @@ InstallGlobalFunction( AUTODOC_INSTALL_TREE_SETTERS,
 end );
 
 AUTODOC_INSTALL_TREE_SETTERS();
+
+InstallMethod( IsEmptyNode,
+               [ IsTreeForDocumentationNode ],
+               
+  function( node )
+    
+    if IsBound( node!.content ) then
+        
+        return ForAll( node!.content, IsEmptyNode );
+        
+    fi;
+    
+    return false;
+    
+end );
+
+InstallMethod( IsEmptyNode,
+               [ IsString ],
+               
+  function( node )
+    
+    return node = "";
+    
+end );
 
 ####################################
 ##

gap/DocumentationTree.gd
@@ -36,7 +36,11 @@ DeclareCategory( "IsTreeForDocumentationNode",
 ##
 ######################################
 
-DeclareFilter( "IsEmptyNode" );
+DeclareOperation( "IsEmptyNode",
+                  [ IsTreeForDocumentationNode ] );
+
+DeclareOperation( "IsEmptyNode",
+                  [ IsString ] );
 
 DeclareAttribute( "Name",
                   IsTreeForDocumentationNode );

commit ad907c83d34160cb6d28820cb09002705d24f2ab

Fixed bug introduced in 96cecb58fe284
Empty chapters and sections should not be printed.
However, there was a bug which caused chapters not to be printed
when only subsections contained text, not a section itself.
Due to the changes, ManItems without description or return value
where not printed anymore.

Closes #80

PackageInfo.g
@@ -6,7 +6,7 @@ PackageName := "AutoDoc",
 Subtitle := "Generate documentation from GAP source code",
 
 Version := Maximum( [
-  "2014.09.11", ## Sebas' version
+  "2014.09.14", ## Sebas' version
 ## This line prevents merge conflicts
   "2014.03.27", ## Max' version
 ## This line prevents merge conflicts

gap/DocumentationTree.gi
@@ -588,6 +588,15 @@ InstallMethod( IsEmptyNode,
     
 end );
 
+InstallMethod( IsEmptyNode,
+               [ IsTreeForDocumentationNodeForManItemRep ],
+               
+  function( node )
+    
+    return false;
+    
+end );
+
 ####################################
 ##
 ## Add functions